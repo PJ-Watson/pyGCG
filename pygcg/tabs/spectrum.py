78643f1ba090f731978315d6a6788d9621f9c371
Peter Watson <59647390+PJ-Watson@users.noreply.github.com>
2024-11-18 12:48:25 +0100

    Add option for step function in 1D spec plot.

--- pygcg/tabs/spectrum.py
+++ pygcg/tabs/spectrum.py
@@ -478,25 +478,63 @@ class SpecFrame(ctk.CTkFrame):
                             data_table["err"][clip] / data_table["flat"][clip] / 1e-19
                         )
 
-                    try:
-                        update_errorbar(
-                            self.plotted_components[dict_key][hdu.name],
-                            data_table["wave"][clip],
-                            y_vals,
-                            yerr=y_err,
-                        )
-                    except:
-                        self.plotted_components[dict_key][
-                            hdu.name
-                        ] = self.fig_axes.errorbar(
-                            data_table["wave"][clip],
-                            y_vals,
-                            yerr=y_err,
-                            fmt="o",
-                            markersize=3,
-                            ecolor=colors.to_rgba(colours[hdu.name], 0.5),
-                            c=colours[hdu.name],
-                        )
+                    if self._root().config["spectrum"]["spec_line"] == "step":
+                        try:
+                            self.plotted_components[dict_key][hdu.name].set_data(
+                                data_table["wave"][clip],
+                                y_vals,
+                            )
+
+                            dummy = self.fig_axes.fill_between(
+                                data_table["wave"][clip],
+                                y_vals - y_err,
+                                y_vals + y_err,
+                                step="mid",
+                                alpha=0,
+                            )
+                            dp = dummy.get_paths()[0]
+                            dummy.remove()
+                            self.plotted_components[dict_key][
+                                f"{hdu.name}_err"
+                            ].set_paths([dp.vertices])
+                        except Exception as e:
+                            (
+                                self.plotted_components[dict_key][hdu.name],
+                            ) = self.fig_axes.plot(
+                                data_table["wave"][clip],
+                                y_vals,
+                                drawstyle="steps-mid",
+                                c=colours[hdu.name],
+                            )
+                            self.plotted_components[dict_key][
+                                f"{hdu.name}_err"
+                            ] = self.fig_axes.fill_between(
+                                data_table["wave"][clip],
+                                y_vals - y_err,
+                                y_vals + y_err,
+                                step="mid",
+                                facecolor=colors.to_rgba(colours[hdu.name], 0.5),
+                            )
+                    else:
+                        try:
+                            update_errorbar(
+                                self.plotted_components[dict_key][hdu.name],
+                                data_table["wave"][clip],
+                                y_vals,
+                                yerr=y_err,
+                            )
+                        except:
+                            self.plotted_components[dict_key][
+                                hdu.name
+                            ] = self.fig_axes.errorbar(
+                                data_table["wave"][clip],
+                                y_vals,
+                                yerr=y_err,
+                                fmt="o",
+                                markersize=3,
+                                ecolor=colors.to_rgba(colours[hdu.name], 0.5),
+                                c=colours[hdu.name],
+                            )
                     ymax = np.nanmax([ymax, np.nanmax(y_vals)])
 
         if not templates:
